6fdd95a3a0e8c80ab381c08e336b71a1e5d46948
Janalp2025 <zillionyoutub@gmail.com>
2026-04-04 01:16:29 +1000

    Initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const ai = require('./ai');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
